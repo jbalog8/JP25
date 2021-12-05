058f1a4396a9d39f0d67f874b6cab0e2ec9ae41e
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2021-12-05 23:37:14 +0100

    Creating database doktor

---
+++ Doktor/doktor.sql
@@ -0,0 +1,39 @@
+drop database if exists doktor;
+create database doktor character set utf8;
+use doktor;
+
+create table doktor(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    placa decimal(18,2),
+    email varchar(50)
+);
+
+create table medicinskasestra(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    placa decimal(18,2),
+    email varchar(50)
+);
+
+create table pacijent(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    mbo char(9) not null,
+    oib char(11) not null
+);
+
+create table lijecenje(
+    sifra int not null primary key auto_increment,
+    doktor int not null,
+    pacijent int not null,
+    trajanje int 
+);
+
+create table medicinskasestra_lijecenje(
+    medicinskasestra int not null,
+    lijecenje int not null
+);
